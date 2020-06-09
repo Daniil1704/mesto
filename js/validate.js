229362a922632780ec76fda6bfaed6da72592512
Daniil Tsyrulnikov <tyron1996sweg@gmail.com>
2020-06-09 13:02:37 +0300

    Мелкие исправления

--- js/validate.js
+++ js/validate.js
@@ -2,24 +2,25 @@ const object = {
     formSelector: '.popup__container',
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__save_disabled',
-    inputErrorClass: 'popup__input-error',
-    errorClass: 'popup__span-error_active'
+    inactiveButtonClass: '.popup__save_disabled',
+    inputErrorClass: '.popup__input-error',
+    errorClass: '.popup__span-error_active'
 };
 
 // показ ошибки 
 const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add('popup__input-error');
+    inputElement.classList.add('.popup__input-error');
     errorElement.textContent = errorMessage;
-    errorElement.classList.add('popup__span-error_active');
+    errorElement.classList.add('.popup__span-error_active');
+    // =
 };
 
 // удалитель ошибки 
 const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.remove('popup__input-error');
-    errorElement.classList.remove('popup__span-error_active');
+    inputElement.classList.remove('.popup__input-error');
+    errorElement.classList.remove('.popup__span-error_active');
     errorElement.textContent = '';
 };
 
@@ -31,6 +32,7 @@ const checkInputValidity = (formElement, inputElement) => {
         hideInputError(formElement, inputElement);
     }
 };
+
 
 const setEventListeners = (formElement) => {
     const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
@@ -59,9 +61,9 @@ const hasInvalidInput = (inputList) => {
 };
 const toggleButtonState = (inputList, buttonElement) => {
     if (hasInvalidInput(inputList)) {
-        buttonElement.classList.add('popup__save_disabled');
+        buttonElement.classList.add('.popup__save_disabled');
     } else {
-        buttonElement.classList.remove('popup__save_disabled');
+        buttonElement.classList.remove('.popup__save_disabled');
     }
 };
 enableValidation(object);
